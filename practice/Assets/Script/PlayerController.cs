62fb35f33698aa1bea66f64f0914cbdee9df451f
Nirperm <nirperm@gmail.com>
2018-10-02 19:33:47 +0900

    Move Player

---
+++ practice/Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    private GameObject rigidbodySource;
+
+    public float speed = 10;
+
+	// Use this for initialization
+	//void Start () {
+
+	//}
+
+	// Update is called once per frame
+	//void Update () {
+
+	//}
+
+	void FixedUpdate(){
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");       
+
+        // 同一のGameObjectが持つRigidbodyコンポーネントを取得
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        // rigidbodyのx軸（横）とz軸（奥）に力を加える
+        rigidbody.AddForce(x * speed, 0, z * speed);
+	}
+}
+
