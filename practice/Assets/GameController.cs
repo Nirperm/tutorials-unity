c3452ebc5b4f5a8c62d701d2940af25a85d1c4ca
Nirperm <nirperm@gmail.com>
2018-10-03 14:33:00 +0900

    Add gemeUI

---
+++ practice/Assets/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEngine.UI.Text scoreLabel;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int count = GameObject.FindGameObjectsWithTag("Item").Length;
+        scoreLabel.text = count.ToString();
+    }
+}
