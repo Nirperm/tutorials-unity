98d9bb57280de8ca9d80eb6221159ea42326c70a
Nirperm <nirperm@gmail.com>
2018-10-03 12:13:57 +0900

    Recover item

---
+++ practice/Assets/Script/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	// トリガーとの接触時に呼ばれるコールバック
+    private void OnTriggerEnter(Collider hit){
+        if (hit.CompareTag("Player")) {
+
+            // このコンポーネントを持つGameObjectを破棄する
+            Destroy(gameObject);
+        }   
+	}
+}
